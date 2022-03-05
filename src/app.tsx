dfe9bb2dd5cc7f6d8c03f48aa910578f4e1d4869
Jack McConnell <jackmc1122@gmail.com>
2022-03-05 07:09:59 -0800

    Get basic react app working

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function render() {
+  ReactDOM.render(<h2>Hello from React! My name is Jack!</h2>, document.body);
+}
+
+render();
