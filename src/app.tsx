20b157ee2da2ba0b8ac3b847002f0547bf33c5c7
Jack McConnell <jackmc1122@gmail.com>
2022-03-05 07:11:32 -0800

    bump

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 function render() {
-  ReactDOM.render(<h2>Hello from React! My name is Jack!</h2>, document.body);
+  ReactDOM.render(<h2>Hello from React! My name is Jack! bump</h2>, document.body);
 }
 
 render();
